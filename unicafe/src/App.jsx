5e525e48719ed77452d02b1250218225b21b5b88
Sandrowue <aleksi.w@protonmail.com>
2026-07-23 13:31:43 +0300

    total feedback toimii

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -7,12 +7,14 @@ const Button = ({onClick, text}) => {
   )
   }
 
-const Display = ({good, ok, poor}) => {
+const Display = ({good, ok, poor, total}) => {
   return (
   <div>
     <p>Good: {good}</p>
     <p>Ok: {ok}</p>
     <p>Poor: {poor}</p>
+    <br />
+    <p>Total: {total}</p>
   </div>
   )
 }
@@ -22,11 +24,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [ok, setOk] = useState(0)
   const [poor, setPoor] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const increaseGood = () => setGood(good + 1);
-  const increaseOk = () => setOk(ok + 1);
-  const increasePoor = () => setPoor(poor +1);
-
+  const increaseGood = () => {
+    setGood(good + 1);
+    const goodSum = good + 1
+    return (
+    goodSum, setTotal(goodSum + ok + poor)
+  )}
+  const increaseOk = () => {
+    setOk(ok + 1);
+    const okSum = ok + 1
+    return (
+    okSum, setTotal(okSum + good + poor)
+    
+  )}
+  const increasePoor = () => {
+    setPoor(poor +1)
+    const poorSum = poor + 1
+    return (
+    poorSum, setTotal(poorSum + good + ok)
+  )}
+  
   return (
     <div>
       <h2>Choose feedback</h2>
@@ -36,7 +55,7 @@ const App = () => {
       <br />
       <br />
       <h2>Statistics</h2>
-      <Display good={good} ok={ok} poor={poor}/>
+      <Display good={good} ok={ok} poor={poor} total={total}/>
     </div>
   )
 }
